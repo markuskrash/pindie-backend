cdb35e2e99785c12de48f03a98809685f45feb1d
markuskrash <markgrechnev@gmail.com>
2024-05-30 04:01:53 +0300

    фронт

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,7 +3,7 @@ const allowedCors = [
     'https://students-projects.ru',
     'localhost:3000',
     'localhost:3001',
-    "https://pindie-frontend.nomoredomainswork.ru"
+    "https://markuskrashfrontend.nomorepartiesco.ru"
 ];
 
 function cors(req, res, next) {
